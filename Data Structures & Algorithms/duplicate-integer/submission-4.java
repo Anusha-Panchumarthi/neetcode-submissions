47e25d1b1a555c3cc3af4eccc2a97829ee5643b9
Anusha Panchumarthi <82055528+Anusha-Panchumarthi@users.noreply.github.com>
2026-08-30 12:24:09 +0530

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i : nums)
+        {
+            set.add(i);
+        }
+
+        return set.size() != nums.length ? true : false;
+    }
+}
